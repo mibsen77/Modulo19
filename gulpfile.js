acf6398d174dd3ff3e3c7f677c151a968a792a90
unknown <mauro.ibsen@gmail.com>
2022-05-13 10:55:53 -0300

    primeiro_commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const cssmin = require('gulp-cssmin')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
+const image = require('gulp-imagemin')
+const stripJs = require('gulp-strip-comments')
+const stripCss = require('gulp-strip-css-comments')
+
+
+function tarefasCSS(cb) {
+
+    return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.css',
+                     './vendor/owl/css/owl.css',
+                     './vendor/fontawesome/fontawesome.css',
+                    './src/css/style.css' ])
+
+        .pipe(concat('styles.css'))
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min'})) // libs.min.css
+        .pipe(gulp.dest('./dist/css'))
+
+}
+
+function tarefasJS(){
+
+    return gulp.src(['./vendor/jquery/jquery-3.6.0.min.js',
+                    './node_modules/jquery/dist/jquery.js',
+                    './node_modules/bootstrap/dist/js/bootstrap.js',
+                    './vendor/owl/js/owl.js',
+                    './src/js/custom.js'])
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min'})) //libs.min.js
+        .pipe(gulp.dest('./dist/js'))
+}
+
+
+function tarefasImagem(){
+    
+    return gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
+}
+
+exports.styles = tarefasCSS
+exports.scripts = tarefasJS
+exports.images = tarefasImagem
